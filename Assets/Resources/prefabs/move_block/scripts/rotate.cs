2aad16f301578bfe344ec071db4f5b675fc8babc
linzexin <linzexinmasterchief@gmail.com>
2019-06-22 10:20:27 +0800

    allow double rotate

--- Assets/Resources/prefabs/move_block/scripts/rotate.cs
+++ Assets/Resources/prefabs/move_block/scripts/rotate.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class rotate : MonoBehaviour
 {
     public bool use_rotate;
+    public Vector3 rotate_around;
+    public Vector3 rotate_axis;
+    private Vector3 rotate_origin;
     public float rotate_speed;
+    public float rotate_speed_around_center;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // rotate around use world coordinate, so add self position to get local position
+        rotate_origin = transform.position + rotate_around;
     }
 
     // Update is called once per frame
@@ -19,6 +24,9 @@ public class rotate : MonoBehaviour
         {
             return;
         }
-        transform.Rotate(new Vector3(0, 0, rotate_speed));
+        // rotate around rotate origin
+        transform.RotateAround(rotate_origin, rotate_axis, rotate_speed);
+        // rotate around center of self
+        transform.Rotate(new Vector3(0, 0, rotate_speed_around_center));
     }
 }
